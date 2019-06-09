860fa8b74513342360fce2e7a5352e7754d10be1
GHST <apkedzior@gmail.com>
2019-06-09 15:09:43 +0200

    Contracts view

--- src/app/contracts/contracts.component.ts
+++ src/app/contracts/contracts.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+declare interface TableData {
+  headerRow: string[];
+  dataRows: string[][];
+}
+
 @Component({
   selector: 'app-contracts',
   templateUrl: './contracts.component.html',
   styleUrls: ['./contracts.component.scss']
 })
 export class ContractsComponent implements OnInit {
+  public tableData1: TableData;
 
   constructor() { }
 
   ngOnInit() {
+    this.tableData1 = {
+      headerRow: [ '#', 'Numer Rejestracyjny', 'Rok', 'Typ', 'Marka', 'Model', 'Opcje'],
+      dataRows: [
+      ['1', 'SWD12345', '2018', 'Prywatny', 'BMW', 'E90', ''],
+      ['2', 'SK15978', '2017', 'Służbowy', 'KIA', 'Ceed', ''],
+      ['3', 'SR45687', '2015', 'Służbowy', 'Volkswagen', 'Passat', ''],
+      ['4', 'KR45678', '2019', 'Służbowy', 'Opel', 'Corsa', ''],
+      ['5', 'SZ25879', '2017', 'Służbowy', 'Chevrolet', 'Cruze', '']
+      ]
+      };
   }
 
 }
